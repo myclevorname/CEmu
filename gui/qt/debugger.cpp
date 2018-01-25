7b5799f5487f3bcf970c27ffcbdf5c57b7fda596
Jacob Young <jacobly0@users.noreply.github.com>
2018-01-25 00:34:15 -0500

    Implement source view and highlight current line.

--- gui/qt/debugger.cpp
+++ gui/qt/debugger.cpp
@@ -789,6 +789,7 @@ void MainWindow::debugPopulate() {
     osUpdate();
     stackUpdate();
     disasmUpdateAddr(m_prevDisasmAddr = cpu.registers.PC, true);
+    ui->sources->updatePC(m_prevDisasmAddr);
 
     memUpdate();
 }
